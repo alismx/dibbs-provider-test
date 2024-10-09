90f0a8dfc5588a5b97d9ef13e1402e04b5d16eea
alismx <alismxwll@gmail.com>
2024-10-09 15:07:25 -0400

    add example setup to pull terraform module from dibbs-aws

---
+++ main.tf
@@ -0,0 +1,36 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name               = local.vpc_name
+  cidr               = var.vpc_cidr
+  azs                = var.availability_zones
+  private_subnets    = var.private_subnets
+  public_subnets     = var.public_subnets
+  enable_nat_gateway = var.enable_nat_gateway
+  single_nat_gateway = var.single_nat_gateway
+  create_igw         = var.create_internet_gateway
+  tags               = local.tags
+}
+
+module "ecs" {
+  source = "github.com/CDCgov/dibbs-aws//terraform/modules/ecs?ref=b80e6ebd89b0aeed0652b4678102378598865a3e"
+
+  public_subnet_ids  = flatten(module.vpc.public_subnets)
+  private_subnet_ids = flatten(module.vpc.private_subnets)
+  vpc_id             = module.vpc.vpc_id
+  region             = var.region
+
+  owner   = var.owner
+  project = var.project
+  tags    = local.tags
+
+  # If intent is to pull from the phdi GHCR, set disable_ecr to true (default is false)
+  # disable_ecr = true
+  # If intent is to use the non-integrated viewer, set non_integrated_viewer to "true" (default is false)
+  # non_integrated_viewer = "true"
+  # If the intent is to make the ecr-viewer availabble on the public internet, set internal to false (default is true) This requires an internet gateway to be present in the VPC.
+  # internal       = false
+  internal       = false
+  non_integrated_viewer = "true"
+  ecr_viewer_app_env = "test"
+}
